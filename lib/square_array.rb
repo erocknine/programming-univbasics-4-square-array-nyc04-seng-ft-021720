39e3b5dd4964f15bfbea2e5529c8e3290afd4375
erocknine <echeungsk@gmail.com>
2020-01-21 16:29:54 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,7 @@
 def square_array(array)
   counter = 0
-  new_array = []
   while counter < array.length do
-    new_array.push(array[counter] **2)
+    array[counter] = array[counter] **2
     counter += 1
   end
-  return new_array
 end
